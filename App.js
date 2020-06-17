a64b6ec024499ee4a47217b317600c37b832cafd
unknown <ihtisham.javed@funavry.com>
2020-06-17 21:12:30 +0500

    first

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  <Text>Bezier Line Chart</Text>
+  <BarChart
+
+  style={graphStyle}
+  data={data}
+  width={300}
+  height={220}
+withHorizontalLabels={false}
+  chartConfig={chartConfig}
+  withInnerLines={false}
+  showValuesOnTopOfBars
+/>
+</View>
   );
 }
 
@@ -17,3 +35,29 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const chartConfig = {
+  backgroundGradientFrom: "white",
+  backgroundGradientFromOpacity: 1,
+  backgroundGradientTo: "white",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `red`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 1.5,
+  useShadowColorFromDataset: false // optional
+
+
+};
+const data = {
+  labels: ["17/06","16/06","15/06"],
+  datasets: [
+    {
+      data: [20, 45, 28]
+    }
+  ]
+};
+const graphStyle ={
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    flex:1, 
+  };
